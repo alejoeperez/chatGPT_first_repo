a99958d184a80edcd2ef1ee7beb81c78262e6917
alejoeperez <alejoe.perez@gmail.com>
2025-05-13 09:04:41 -0500

    Creating User Class with basic attributes, adding encapsulation property to the attribtues, Adding getters and setters for accessing to the attributes of User Java Class, adding comment to toString override method

---
+++ User.java
@@ -0,0 +1,40 @@
+public class User{
+
+    private String id;
+    private String name;
+    private int age;
+
+    //GETTERS AND SETTERS
+    public String getId(){
+        return this.id;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+
+    public String geName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    /*This is for toString method including
+     * id, name, age
+     */
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
+    
+}
